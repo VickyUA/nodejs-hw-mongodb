08caee44fa0a0d250671805593b89c330b6648bf
VickyUA <vickyy379@gmail.com>
2025-07-24 17:38:08 +0300

    'update'

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,10 @@
 import { Router } from 'express';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import {
   getContactsController,
   getContactByIdController,
@@ -12,12 +18,29 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
