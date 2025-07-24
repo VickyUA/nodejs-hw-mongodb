90ce737eaf938ef3cdcb398ce5255b95249e882a
VickyUA <vickyy379@gmail.com>
2025-07-24 23:20:05 +0300

    'update'

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,6 @@
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 import {
   getAllContacts,
   getContactById,
@@ -8,7 +11,17 @@ import {
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
 
   res.json({
     status: 200,
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,18 @@
+const parseType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+  const isType = (type) => ['work', 'home', 'personal'].includes(type);
+
+  if (isType(type)) return type;
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+
+  const parsedType = parseType(type);
+
+  return {
+    type: parsedType,
+    isFavourite: isFavourite,
+  };
+};
