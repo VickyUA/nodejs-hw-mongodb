7c21fbb721c7f4675e0d3e7b9eab527200557550
VickyUA <vickyy379@gmail.com>
2025-07-27 20:44:48 +0300

    'update'

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from '../src/routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
   app.use(express.json());
-
   app.use(
     pino({
       transport: {
@@ -19,10 +19,10 @@ export const setupServer = () => {
       },
     }),
   );
-
   app.use(cors());
+  app.use(cookieParser());
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
